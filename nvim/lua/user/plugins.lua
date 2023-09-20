3c7c5017b2716d0a3a9a451dc0580906cc5e413d
andrew malinnikov <code.bright.anywhere@gmail.com>
2023-09-20 22:35:51 +0300

    file navigation plugins

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -132,6 +132,36 @@ use({
   end,
 })
 
+use({
+    'nvim-telescope/telescope.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'kyazdani42/nvim-web-devicons',
+      'nvim-telescope/telescope-live-grep-args.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    },
+    config = function()
+      require('user/plugins/telescope')
+    end
+  })
+
+-- File tree sidebar
+use({
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('user/plugins/nvim-tree')
+    end
+  })
+
+use({
+    'nvim-lualine/lualine.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('lualine').setup()
+    end
+  })
+
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
 if packer_bootstrap then
---
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -0,0 +1,18 @@
+require('nvim-tree').setup({
+    git = {
+      ignore = false,
+    },
+    renderer = {
+      group_empty = false,
+      icons = {
+        show = {
+          folder_arrow = false
+        },
+      },
+      indent_markers = {
+        enable = true
+      },
+    },
+  })
+
+vim.keymap.set('n', '<Leader>1', ':NvimTreeFindFileToggle<CR>')
